de82f0d5b03d0e349d304dc69051e3af7aaa06cb
yuyongxin <970104685@qq.com>
2017-02-15 23:28:30 +0800

    ok

--- utils.js
+++ utils.js
@@ -217,6 +217,10 @@ var utils = (function () {
         }
     }
 
+    /**
+     * 设置css 
+     * @param curEle
+     */
     function css(curEle) {
         var arg2 = arguments[1];
         if (typeof arg2 === 'string') {
